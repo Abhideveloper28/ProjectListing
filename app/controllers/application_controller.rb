661a6c8e0d9687125e7154303ceb64f0dfaa84fd
Abhi <abhi@developer.com>
2022-10-19 18:21:06 +0530

    HTTP authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: Rails.application.credentials.HTTP_USER, password: Rails.application.credentials.HTTP_PASS
 end
+
